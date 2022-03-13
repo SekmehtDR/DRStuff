4c5d7c747c75da6799b146ad6e956e1e633ee1d5
SekmehtDR <50120656+SekmehtDR@users.noreply.github.com>
2022-03-13 16:55:10 -0700

    Update GenieInstaller-Beta.ps1

--- GenieInstaller-Beta.ps1
+++ GenieInstaller-Beta.ps1
@@ -11,11 +11,16 @@ $gitHubRepo = $Matches.GithubRepo
 $gitHubVersion = $Matches.Version
 $genieGitURL = "https://github.com/$gitHubUser/$gitHubRepo/releases/download/$gitHubVersion/"
 
-#Where to create the genie folder at (Default)?
+#GenieMaps Location:
+$genieMapsURL = "https://github.com/GenieClient/Maps/archive/refs/heads/"
+$genieMapsGitHubZipfile = "main.zip"
+$genieMapsFileURL = "$genieMapsGitHubURL$genieMapsGitHubZipfile"
+
+#Where to create the genie folder at (Default: C:\temp\)?
 $genieInstallRootFolder = "C:\temp\"
 
 #Where to migrate Genie folder from? (Default)
-$genieMigrateRootFolder = "C:\temp\Genie-XXXX"
+$genieMigrateRootFolder = ""
 
 #Formatting of Genie FolderName (Genie Client <version without periods>. Ex. Genie Client 4020).
 $genieVersionName = $gitHubVersion.Replace(".", "")
@@ -24,6 +29,7 @@ $genieFolderName = "Genie-$genieVersionName"
 #Name of files in Repo, package specific stuff will be generated using a switch below
 $genieConfigFiles = "Base.Config.Files.zip"
 $geniePlugins = "Plugins.zip"
+$genieMaps = "main.zip"
 
 #Parent Installation Path, change $genieInstallRootFolder
 $fullGenieFolderPath = "$genieInstallRootFolder$genieFolderName\"
@@ -37,6 +43,17 @@ function PromptYesNo {
     while ("yes","no" -notcontains $yorn_answer)
     {
         $yorn_answer = Read-Host "Continue? (Yes/No)"
+    }
+        if ($yorn_answer -eq "no"){
+            Write-Host "Exiting..." -ForegroundColor Red
+        exit
+        }
+}
+
+function installReadyYN {
+    while ("yes","no" -notcontains $yorn_answer)
+    {
+        $yorn_answer = Read-Host "Ready to Install? (Yes/No)"
     }
         if ($yorn_answer -eq "no"){
             Write-Host "Exiting..." -ForegroundColor Red
@@ -78,7 +95,7 @@ if ($deploymentOption -eq "Install"){
     Write-Host "-----------------------------------------------------"  -ForegroundColor Green
     Write-Host "Option 1: Install - NEW - Genie $gitHubVersion"
     Write-Host "-----------------------------------------------------"  -ForegroundColor Green
-    Write-Host "GITHUB URL: $genieGitURL"
+    Write-Host "G4 URL: $genieGitURL"                                   -ForegroundColor Yellow
     Write-Host ""                                                       -ForegroundColor Green
     Write-Host "Available Builds:"                                      -ForegroundColor Yellow
     Write-Host "- Option 1: x86"                                        -ForegroundColor Yellow
@@ -106,9 +123,12 @@ if ($deploymentOption -eq "Install"){
     Write-Host ""
     Write-Host "-----------------------------------------------------"  -ForegroundColor Green
     Write-Host ""
-    Write-Host "GITHUB URL:             $genieGitURL"                   -ForegroundColor Yellow
+    Write-Host "G4 URL:                 $genieGitURL"                   -ForegroundColor Yellow
+    write-host "G4 Maps URL:            $genieMapsURL"                  -ForegroundColor Yellow
+    Write-Host ""
     Write-Host "Config Package:         $genieConfigFiles"              -ForegroundColor Yellow
     write-Host "Plugins:                $geniePlugins"                  -ForegroundColor Yellow
+    write-Host "GenieMaps:              $genieMapsGitHubZipfile"        -ForegroundColor Yellow
     Write-Host "Application Package:    $geniePackage"                  -ForegroundColor Yellow
     if ($buildanswer -eq "3") {
         Write-Host "Desktop Runtime:        $windowsdesktopruntimeFilename" -ForegroundColor Yellow
@@ -117,20 +137,14 @@ if ($deploymentOption -eq "Install"){
     Write-Host "Install Path:           $fullGenieFolderPath"           -ForegroundColor Yellow
     Write-Host ""
     Write-Host "-----------------------------------------------------"  -ForegroundColor Green
-
-    #verification of some config
-    PromptYesNo
+    installReadyYN
     Write-Host "-----------------------------------------------------"  -ForegroundColor Green  
     #makes the new folder
     Write-Host ""
     Write-Host "Making new directory:   $fullGenieFolderPath" -ForegroundColor Yellow
     Write-Host ""
-    Write-Host "-----------------------------------------------------"  -ForegroundColor Green
-    PromptYesNo
-    Write-Host "-----------------------------------------------------"  -ForegroundColor Green
     #if folder exists, see if its empty.. if not exit. If folder exists but is empty, continue. If older is new make it!
     if (Test-Path $fullGenieFolderPath) {
-        Write-Host ""
         Write-Host "Folder already exists..." -ForegroundColor Yellow
         $folderfilecount = (Get-ChildItem -Path $fullGenieFolderPath -File | Measure-Object).Count
         if ($folderfilecount -gt 0){
@@ -151,22 +165,23 @@ if ($deploymentOption -eq "Install"){
     Write-Host ""
     Write-Host "Downloading files via Invoke-WebRequest..."             -ForegroundColor Yellow
     Write-Host ""
-    Write-Host "GITHUB URL:             $genieGitURL"                   -ForegroundColor Yellow
+    Write-Host "G4 URL:                 $genieGitURL"                   -ForegroundColor Yellow
+    write-host "G4 Maps URL:            $genieMapsURL"              -ForegroundColor Yellow
+    Write-Host ""
     Write-Host "Config Package:         $genieConfigFiles"              -ForegroundColor Yellow
     write-Host "Plugins:                $geniePlugins"                  -ForegroundColor Yellow
+    write-Host "GenieMaps:              $genieMapsGitHubZipfile"        -ForegroundColor Yellow
     Write-Host "Application Package:    $geniePackage"                  -ForegroundColor Yellow
     if ($buildanswer -eq "3") {
         Write-Host "Desktop Runtime:        $windowsdesktopruntimeFilename" -ForegroundColor Yellow
     }
     Write-Host ""
-    Write-Host "-----------------------------------------------------"  -ForegroundColor Green  
-    PromptYesNo
-    Write-Host "-----------------------------------------------------"  -ForegroundColor Green  
-    Write-Host ""
     Write-Host "Downloading $genieConfigFiles to $fullGenieFolderPath"   -ForegroundColor Yellow
     Invoke-WebRequest -Uri "$genieGitURL$genieConfigFiles" -OutFile "$fullGenieFolderPath$genieConfigFiles"
     Write-Host "Downloading $geniePlugins to $fullGenieFolderPath"   -ForegroundColor Yellow
     Invoke-WebRequest -Uri "$genieGitURL$geniePlugins" -OutFile "$fullGenieFolderPath$geniePlugins"
+    Write-Host "Downloading $genieMaps to $fullGenieFolderPath"   -ForegroundColor Yellow
+    Invoke-WebRequest -Uri "$genieMapsURL$genieMapsGitHubZipfile" -OutFile "$fullGenieFolderPath$genieMapsGitHubZipfile"
     Write-Host "Downloading $geniePackage to $fullGenieFolderPath"   -ForegroundColor Yellow
     Invoke-WebRequest -Uri "$genieGitURL$geniePackage" -OutFile "$fullGenieFolderPath$geniePackage"
     if ($buildanswer -eq "3") {
@@ -175,20 +190,19 @@ if ($deploymentOption -eq "Install"){
     }
     Write-Host "" 
     Write-Host "File download completed..." -ForegroundColor Green
-    #extracts the stuff in order
     Write-Host "" 
     Write-Host "-----------------------------------------------------"  -ForegroundColor Green 
     Write-Host "" 
     Write-Host "Extract the ZIP files to:   $fullGenieFolderPath ?"   -ForegroundColor Yellow
     Write-Host "" 
-    Write-Host "-----------------------------------------------------"  -ForegroundColor Green 
-    PromptYesNo
-    Write-Host "-----------------------------------------------------"  -ForegroundColor Green 
-    Write-Host "" 
     Write-Host "Extracting $genieConfigFiles to $fullGenieFolderPath"   -ForegroundColor Yellow
     Expand-Archive -LiteralPath "$fullGenieFolderPath$genieConfigFiles" -DestinationPath "$fullGenieFolderPath"
     Write-Host "Extracting $geniePlugins to $fullGenieFolderPath"   -ForegroundColor Yellow
     Expand-Archive -LiteralPath "$fullGenieFolderPath$geniePlugins" -DestinationPath "$fullGenieFolderPath\Plugins" -Force
+    Write-Host "Cleaning Up - Maps Directory in $fullGenieFolderPath"   -ForegroundColor Yellow
+    Remove-Item "$fullGenieFolderPath\Maps\*"  -Recurse -Force
+    Write-Host "Extracting $genieMaps to $fullGenieFolderPath"   -ForegroundColor Yellow
+    Expand-Archive -LiteralPath "$fullGenieFolderPath$genieMapsGitHubZipfile" -DestinationPath "$fullGenieFolderPath\Maps"
     Write-Host "Extracting $geniePackage to $fullGenieFolderPath"   -ForegroundColor Yellow
     Expand-Archive -LiteralPath "$fullGenieFolderPath$geniePackage" -DestinationPath "$fullGenieFolderPath"
     Write-Host "" 
@@ -205,27 +219,32 @@ if ($deploymentOption -eq "Install"){
         }
     }
 
-    Write-Host ""
-    Write-Host "-----------------------------------------------------"  -ForegroundColor Green 
     #deletes the files
+    Write-Host "-----------------------------------------------------"  -ForegroundColor Green 
     Write-Host ""
     Write-Host "Cleanup: Removing the ZIP Files downloaded in $fullGenieFolderPath"   -ForegroundColor Yellow
-    Write-Host ""
-    Write-Host "-----------------------------------------------------"  -ForegroundColor Green
-    PromptYesNo
-    Write-Host "-----------------------------------------------------"  -ForegroundColor Green
     Write-Host ""
     Write-Host "Removing $genieConfigFiles from $fullGenieFolderPath"   -ForegroundColor Yellow
     remove-item $fullGenieFolderPath$genieConfigFiles -Force
     Write-Host "Removing $geniePlugins from $fullGenieFolderPath"   -ForegroundColor Yellow
     remove-item $fullGenieFolderPath$geniePlugins -Force
+    Write-Host "Removing $genieMaps from $fullGenieFolderPath"   -ForegroundColor Yellow
+    remove-item $fullGenieFolderPath$genieMaps -Force
     Write-Host "Removing $geniePackage from $fullGenieFolderPath"   -ForegroundColor Yellow
     remove-item $fullGenieFolderPath$geniePackage -Force
     Write-Host "" 
     Write-Host "Downloaded file cleanup completed..." -ForegroundColor Green
-    Write-Host ""
     Write-Host "-----------------------------------------------------"  -ForegroundColor Green 
     Write-Host ""
+    Write-Host "Post-Install processing..."   -ForegroundColor Yellow
+    Write-Host ""
+    Write-Host "GenieMap Update/Cleanup"   -ForegroundColor Yellow
+    Copy-Item "$fullGenieFolderPath\Maps\Maps-main\*" "$fullGenieFolderPath\Maps" -Recurse -Force
+    Remove-Item "$fullGenieFolderPath\Maps\Maps-main"  -Recurse -Force
+    Write-Host "" 
+    Write-Host "Post-Install processing completed..." -ForegroundColor Green
+    Write-Host "-----------------------------------------------------"  -ForegroundColor Green 
+    Write-Host "" 
     if ($buildanswer -eq "3") {
         Write-Host "IMPORTANT: Option 3: x64 Runtime Dependent Related" -ForegroundColor Yellow
         Write-Host "Please make sure to run $windowsdesktopruntimeFilename prior to running Genie.exe!" -ForegroundColor Yellow
