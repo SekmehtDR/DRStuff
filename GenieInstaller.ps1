494ad2b3428a563c413d87bf28bec1600db110dd
SekmehtDR <50120656+SekmehtDR@users.noreply.github.com>
2022-03-13 11:06:33 -0700

    Update GenieInstaller.ps1

--- GenieInstaller.ps1
+++ GenieInstaller.ps1
@@ -1,6 +1,5 @@
-#         By Sekmeht Usho
-# https://github.com/SekmehtDR/DRstuff
-#           13MAR2021
+#   By Sekmeht Usho - 13MAR2021
+#   https://github.com/SekmehtDR/DRstuff
 
 #Github Release URL Info
 $releaseDownloadURL = "https://github.com/GenieClient/Genie4/releases/download/4.0.2.1/"
